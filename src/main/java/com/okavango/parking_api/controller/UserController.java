9923a4d917ccf77308e5ec2cc19f90c691db1e81
Edvaldo Leite <eguilhermeleite@gmail.com>
2023-11-22 15:10:58 -0300

    created User repository, service & controller

---
+++ src/main/java/com/okavango/parking_api/controller/UserController.java
@@ -0,0 +1,16 @@
+package com.okavango.parking_api.controller;
+
+import com.okavango.parking_api.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/users")
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+
+
+}
---
+++ src/main/java/com/okavango/parking_api/service/UserService.java
@@ -0,0 +1,12 @@
+package com.okavango.parking_api.service;
+
+import com.okavango.parking_api.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserRepository userRepository;
+}
